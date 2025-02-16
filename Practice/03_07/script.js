cc5989a5570b524652878f1bd00461b647d2e0aa
Riyaa-tech <riyaasonawane@gmail.com>
2025-02-16 11:24:09 +0530

    Update script.js
    The script is updated based on an object "bottle" having various attributes, a method "changeStatus" and a nested object "cap"

--- Practice/03_07/script.js
+++ Practice/03_07/script.js
@@ -7,3 +7,27 @@
  * - Find an object that has another object inside of it to create a nested object.
  * - Test your objects in the browser console by accessing the entire object and its specific properties.
  */
+
+const bottle = {
+  name: "Water-Bottle",
+  volume: 1000,
+  color: "orange",
+  capStatus: false,
+  cap: {
+    shape: "round",
+    color: "red",
+  }
+  changeStatus: function(capStatus)
+    {
+      if(capStatus)
+      {
+        capStatus = !capStatus;
+        return "Now the bottle is closed";
+      }
+      else
+      {
+        capStatus = !capStatus;
+        return "Now the bottle is opened";
+      }
+    },
+};
