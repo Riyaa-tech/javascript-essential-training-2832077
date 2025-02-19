8fe4d8e80a00181c19481eb5513bf35bee21f5d5
Riyaa-tech <riyaasonawane@gmail.com>
2025-02-19 14:10:08 +0530

    Update script.js
    The changes made highlight around the entire grid when you hover over it with your mouse, highlight that cell when you hover your mouse, changes grid cell background color when it is clicked and changes the background color of the whole page when a specific key on the keyboard is clicked

--- Practice/09_04/script.js
+++ Practice/09_04/script.js
@@ -5,3 +5,57 @@
  * - Add an event listener to each grid cell to change its background color when it is clicked.
  * - Add an event listener to a specific key on the keyboard to change the background color of the whole page - from dark to light and back again.
  */
+const cell = document.querySelector(".cell");
+const grid = document.querySelector(".grid");
+
+grid.addEventListener(
+  "mouseenter",
+  () => {
+    grid.classList.add("coloredgrid");
+  },
+  false
+);
+
+grid.addEventListener(
+  "mouseleave",
+  () => {
+    grid.classList.remove("coloredgrid");
+  },
+  false
+);
+
+cell.addEventListener(
+  "mouseenter",
+  () => {
+    cell.classList.add("coloredcell");
+  },
+  false
+);
+
+cell.addEventListener(
+  "mouseleave",
+  () => {
+    cell.classList.remove("coloredcell");
+  },
+  false
+);
+
+cell.addEventListener(
+  "click",
+  () => {
+    cell.classList.toggle("bgcolorset");
+  },
+  false
+);
+
+document.addEventListener(
+  "keypress",
+  () => {
+    if (event.key === 'Enter')
+    {
+      document.body.style.backgroundColor = (document.body.style.backgroundColor === "white" ? 
+                document.body.style.backgroundColor = "black" : document.body.style.backgroundColor = "white");
+    }
+  },
+  false
+);
